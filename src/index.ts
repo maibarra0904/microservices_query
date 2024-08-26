a197e7c03462a75781ae6771c55cb274a61dce89
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-08-26 05:03:37 -0500

    Configuración para que en caso de que este servicio de query deje de funcionar, una vez que lo haga cargue toda la data incluida la de eventos creados cuando este servicio estaba fuera de linea

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import cors from "cors"
+import axios from 'axios';
 
 
 const app = express();
@@ -14,50 +15,63 @@ interface PostItem {
     id: string;
     title: string;
     comments: any[];
-  }
+}
 
-const posts: PostItem[] = []
-app.get('/posts', (req,res) => {
-    res.send(posts)
-})
 
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
 
-    if(type === "POST_CREATED") {
-        const {id, title} = data;
+let posts = []
 
-        posts.push({id, title, comments: []})
+const handleEvent = (type, data) => {
+    if (type === "POST_CREATED") {
+        const { id, title } = data;
 
-    }
-
-    if(type === "COMMENT_CREATED") {
-        const {commentId, comments, idPost} = data;
-        const currentPost = posts.filter(post => post.id === idPost)
-
-        currentPost[0].comments.push({id:commentId, content:comments, status:''})
+        posts.push({ id, title, comments: [] })
         console.log(posts)
     }
 
-    if(type === "COMMENT_MODERATED") {
-        const {commentId, comments, idPost, status} = data;
+    if (type === "COMMENT_CREATED") {
+        const { commentId, comments, idPost } = data;
+        const currentPost = posts.filter(post => post.id === idPost)
+
+        currentPost[0].comments.push({ id: commentId, content: comments, status: '' })
+        console.log(posts)
+    }
+
+    if (type === "COMMENT_MODERATED") {
+        const { commentId, comments, idPost, status } = data;
         posts.map(post => {
-            if(post.id === idPost) {
+            if (post.id === idPost) {
                 const updatedComment = post.comments.find(comment => comment.id === commentId)
-                if(updatedComment) {
+                if (updatedComment) {
                     updatedComment.status = status;
                 }
             }
         })
-        
+
         console.log(posts)
     }
-    
-    res.send({status: "Ok"});
+}
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data)
+
+    res.send({ status: "Ok" });
 });
 
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
-  });
-  
+
+    const res = await axios.get('http://localhost:4005/events')
+
+    for (let event of res.data) {
+        console.log('Processing event:', event.type)
+        handleEvent(event.type, event.data)
+    }
+
+});
